166755aa3793ebfd557c0e9161c724cdf4c6b8c4
Patryk Kalinowski <kalinowski1999@gmail.com>
2022-04-11 05:49:06 +0200

    Make few changes so installed app actually works.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ir_planner
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
+            //temporary fix so .exe can connect to database without changing folder/file permissions
+            //if (IsAdministrator() == false)
+            //{
+            //    MessageBox.Show("Run as Administrator", "DataBase Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    return;
+            //}
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
